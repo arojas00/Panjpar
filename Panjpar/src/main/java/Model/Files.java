cb38528b8470680b563d3dd03b455bc079e4ec5b
Javier <javidonatoh01@gmail.com>
2022-07-07 12:49:40 -0600

    Inicio de ReadFile

--- Panjpar/src/main/java/Model/Files.java
+++ Panjpar/src/main/java/Model/Files.java
@@ -6,7 +6,9 @@ package Model;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -53,6 +55,25 @@ public class Files {
             myWriter.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
+        }
+    }
+    
+    public void readFile(Player one, Player two, Deck deck, Boolean round, 
+            Boolean attacker){
+        try{
+            File myObj = new File("../SaveFiles/PanjparGame"+(0)+".txt");
+            Scanner myReader = new Scanner(myObj);
+            while(myReader.hasNextLine()){
+                if(myReader.nextLine().equals("PlayerOne")){
+                    one.setId(myReader.nextLine());
+         
+                }
+                
+            }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            System.out.println("An error Occurred.");
+            e.printStackTrace();
         }
     }
     
