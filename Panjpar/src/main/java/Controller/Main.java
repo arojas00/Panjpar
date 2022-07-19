8fbd68791714abc4916f05dcacd102bd3bf14965
Jafet-Picado <jafethpqpq26@gmail.com>
2022-07-19 16:06:04 -0600

    Se crea clase Main
    Se crea la clase Main para poder crear y ejecutar el juego

---
+++ Panjpar/src/main/java/Controller/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author jafet
+ */
+public class Main {
+    public static void main(String[] args){
+        Game game = new Panjpar();
+        game.run();
+    }
+}
