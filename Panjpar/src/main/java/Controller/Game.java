c15f7a135c13b6a91a912bf21718ce0a6263cacc
Jafet-Picado <jafethpqpq26@gmail.com>
2022-07-19 16:04:59 -0600

    Se actualiza la clase Game
    Se añaden metodos compartidos para todos los controladores que hereden de Game

--- Panjpar/src/main/java/Controller/Game.java
+++ Panjpar/src/main/java/Controller/Game.java
@@ -4,10 +4,135 @@
  */
 package Controller;
 
+import Model.Files;
+import Model.Validator;
+import View.MainView;
+import View.WhoStartsView;
+import java.util.ArrayList;
+
 /**
  *
  * @author jafet
+ * @param <TypeOfDeck>
+ * @param <TypeOfPlayer>
  */
-public class Game {
+public abstract class Game<TypeOfDeck, TypeOfPlayer> {
+    public TypeOfPlayer playerOne;
+    public TypeOfPlayer playerTwo;
+    public ArrayList<TypeOfDeck> decks;
+    public WhoStartsView viewP;
+    public MainView viewM;
+    public Boolean round;
+    public Files files;
+    public Validator validator;
+
+    public TypeOfPlayer getPlayerOne() {
+        return playerOne;
+    }
+
+    public void setPlayerOne(TypeOfPlayer playerOne) {
+        this.playerOne = playerOne;
+    }
+
+    public TypeOfPlayer getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public void setPlayerTwo(TypeOfPlayer playerTwo) {
+        this.playerTwo = playerTwo;
+    }
+
+    public ArrayList<TypeOfDeck> getDecks() {
+        return decks;
+    }
+
+    public void setDecks(ArrayList<TypeOfDeck> decks) {
+        this.decks = decks;
+    }
+
+    public WhoStartsView getViewP() {
+        return viewP;
+    }
+
+    public void setViewP(WhoStartsView viewP) {
+        this.viewP = viewP;
+    }
+
+    public MainView getViewM() {
+        return viewM;
+    }
+
+    public void setViewM(MainView viewM) {
+        this.viewM = viewM;
+    }
+
+    public Boolean getRound() {
+        return round;
+    }
+
+    public void setRound(Boolean round) {
+        this.round = round;
+    }
+
+    public Files getFiles() {
+        return files;
+    }
+
+    public void setFiles(Files files) {
+        this.files = files;
+    }
     
+    public abstract void setWhoStarts(int init);
+    
+    /**
+     * Metodo ejecuta el juego
+     */
+    public void run() {
+        prepareDeck();
+        viewM.updateTrumpCard();
+        viewM.setVisible(true);
+        viewM.enable(false);
+        viewP.setVisible(true);
+        startGame();
+    } 
+    
+    /**
+     * Inicializa el juego
+     */
+    public abstract void startGame();
+    
+        /**
+     * Metodo prepara el deck
+     */
+    public abstract void prepareDeck();
+    
+    public abstract Boolean checkPlay();
+    
+    public void saveGame(){
+        if(files.saveGame()){
+            this.viewM.saved();
+        } else {
+            this.viewM.showFileError();
+        }
+    }
+   
+    
+    /**
+     * Metodo para cargar el estado de un juego
+     * @param fileName String de numero de id del archivo
+     */
+    public void loadGame(String fileName){
+        if(files.readFile(fileName)){
+            this.viewM.loaded();
+            this.viewM.updateUI();
+        } else {
+            this.viewM.showFileError();
+        }
+    }
+    /**
+     * Metodo que invierte el valor de round
+     */
+    public void changeRound(){
+        this.round = !this.round;
+    }
 }
